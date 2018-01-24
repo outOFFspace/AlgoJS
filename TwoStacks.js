429fb38bccb3eecc005a1ea5f1e3dcd523290258
Bohdan Vovchuk <vovchuck.bogdan@gmail.com>
2018-01-24 22:51:19 +0200

    Update TwoStacks.js

--- TwoStacks.js
+++ TwoStacks.js
@@ -1,7 +1,7 @@
 /**
 In this challenge, you must first implement a queue using two stacks. Then process  queries, where each query is one of the following  types:
 
-1 x: Enqueue element  into the end of the queue.
+1: Enqueue element  into the end of the queue.
 2: Dequeue the element at the front of the queue.
 3: Print the element at the front of the queue.
 */
