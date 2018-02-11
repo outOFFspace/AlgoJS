12bc38b25c29ec2f859b3c383b07252499df5add
Bohdan Vovchuk <vovchuck.bogdan@gmail.com>
2018-02-11 19:57:42 +0200

    Create ArraySequences.js

---
+++ ArraySequences.js
@@ -0,0 +1,31 @@
+/**
+Given an array of integers (sorted), find the sequences of integers. e.g. [4,5,7,9,10,12] should return [4,5], [7], [9,10],[12]
+*/
+
+
+function findSequences(a) {
+  const res = {}
+  let seqI = 0
+
+  for (let i = 0; i < a.length; i++) {
+    if (!res[seqI]) {
+      res[seqI] = []
+    }
+    if (!a[i - 1]) {
+      res[seqI] = [a[i]]
+      continue
+    }
+    if (a[i] - a[i - 1] == 1) {
+      res[seqI].push(a[i])
+    } else {
+      // create new sequence
+      seqI++
+      res[seqI] = [a[i]]
+    }
+  }
+
+  return res
+}
+
+console.log(findSequences([4, 5, 7, 9, 10, 12]))
+console.log(findSequences([1, 2, 3, 5, 7, 9, 10, 12]))
